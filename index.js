4acd659c44c4ab11d1fdfa7f664897bbf9ce8b7f
Aman Raj <113578582+huamanraj@users.noreply.github.com>
2025-02-01 01:14:30 +0530

    bug fix

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import cors from 'cors';
 dotenv.config();
 
 
+
+
 // Initialize Appwrite client
 const client = new Client()
   .setEndpoint(process.env.APPWRITE_ENDPOINT)
@@ -22,6 +24,12 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 
